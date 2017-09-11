6b0202e166ed5db7ca8c64adda1a752f95bb2e09
James Aung <therealaung@gmail.com>
2017-09-11 18:46:16 +0100

    move spreadsheet code to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'bundler'
+Bundler.require
+
+# Authenticate a session with your Service Account
+session = GoogleDrive::Session.from_service_account_key("secrets/client_secret.json")
+
+# Get the spreadsheet by its key. Select first worksheet.
+spreadsheet = session.spreadsheet_by_key("17njnrWd-YQcXNkRJjA6aRmtLkoxyeMeldW6xXB3mEkk").worksheets[0]
+
+# Print out the first 6 columns of each row
+spreadsheet.rows.each { |row| puts row.first(2).join(" | ") }
