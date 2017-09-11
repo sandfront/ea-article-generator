4caf34217430e1fba82998ab3ba1333838fec9f7
James Aung <therealaung@gmail.com>
2017-09-11 22:50:55 +0100

    added random functionality and style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,9 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def random
+    @article = Article.order("RANDOM()").first
+    render "show"
+  end
 end
