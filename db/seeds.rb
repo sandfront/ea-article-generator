a4b48d0cebed2a1ab213f4787c1e0b8a2a1e93f0
James Aung <therealaung@gmail.com>
2017-09-12 14:37:07 +0100

    reconfig env

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,15 @@ require 'bundler'
 Bundler.require
 
 # Authenticate a session with your Service Account
-session = GoogleDrive::Session.from_service_account_key("secrets/client_secret.json")
+session = GoogleDrive::Session.from_service_account_key(JSON.parse(ENV['GOOGLE_CLIENT_SECRETS'])
 
 # Get the spreadsheet by its key. Select first worksheet.
 spreadsheet = session.spreadsheet_by_key("17njnrWd-YQcXNkRJjA6aRmtLkoxyeMeldW6xXB3mEkk").worksheets[0]
 
+Article.destroy_all
+
 # Print out the first 6 columns of each row
-spreadsheet.rows.drop(1).take(5).each do |row|
+spreadsheet.rows.drop(1).each do |row|
   p Article.create(title: row[0], author: row[1], url: row[2])
 end
+f
