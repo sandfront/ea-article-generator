0c388a0ab69f9c0ca0bf934848d406ad710e32c2
James Aung <therealaung@gmail.com>
2018-07-18 16:30:15 +0100

    cleaned up routes to only use root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  resources :articles, only: [:show, :index]
-  get '/random', to: 'articles#random', as: 'random'
+  root to: 'articles#random'
+  # resources :articles, only: [:show, :index]
+  # get '/random', to: 'articles#random', as: 'random'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
